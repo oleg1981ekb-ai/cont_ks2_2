18fe5a318462cbe1fe8c0cd980cb4ed53d05b58f
ТЕСТ <test@Oleg.local>
2026-06-30 07:52:41 +0300

    Moved Payment column next to 1exP

--- formatter.py
+++ formatter.py
@@ -7,10 +7,11 @@ def apply_conditional_formatting(ws):
     f_ylw = config.PatternFill(start_color="FFEB9C", end_color="FFEB9C", fill_type="solid")
     f_red = config.PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type="solid")
     font_grn, font_ylw, font_red = Font(name="Calibri", size=11, color="006100"), Font(name="Calibri", size=11, color="9C6500"), Font(name="Calibri", size=11, color="9C0006")
-    # Цветовые статусы теперь занимают ячейки D, E, F, G, H
-    ws.conditional_formatting.add("D2:H1000", CellIsRule(operator='equal', formula=['1'], fill=f_grn, font=font_grn))
-    ws.conditional_formatting.add("D2:H1000", CellIsRule(operator='equal', formula=['2'], fill=f_ylw, font=font_ylw))
-    ws.conditional_formatting.add("D2:H1000", CellIsRule(operator='equal', formula=['3'], fill=f_red, font=font_red))
+    
+    # ДИАПАЗОН РАСШИРЕН ДО СТОЛБЦА I (теперь охватывает D, E, F, G, H, I)
+    ws.conditional_formatting.add("D2:I1000", CellIsRule(operator='equal', formula=['1'], fill=f_grn, font=font_grn))
+    ws.conditional_formatting.add("D2:I1000", CellIsRule(operator='equal', formula=['2'], fill=f_ylw, font=font_ylw))
+    ws.conditional_formatting.add("D2:I1000", CellIsRule(operator='equal', formula=['3'], fill=f_red, font=font_red))
 
 def append_specifications(ws, current_row):
     current_row += 2
@@ -26,7 +27,7 @@ def append_specifications(ws, current_row):
 def set_column_widths(ws):
     ws.column_dimensions['A'].width, ws.column_dimensions['B'].width, ws.column_dimensions['C'].width = 8, 55, 16
     ws.column_dimensions['D'].width = ws.column_dimensions['E'].width = ws.column_dimensions['F'].width = ws.column_dimensions['G'].width = ws.column_dimensions['H'].width = 7
-    # Столбец I (Опл.) делаем компактным (ширина 10)
+    # Столбец I (Опл.)
     ws.column_dimensions['I'].width = 10
-    # Столбец J (Текущий статус акта) делаем широким (ширина 40)
+    # Столбец J (Текущий статус акта)
     ws.column_dimensions['J'].width = 40
